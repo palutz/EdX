99d3dc83f9e12a207fdaa86a45f0424501eeb923
palutz <stefano.paluello@gmail.com>
2012-10-26 00:25:32 +0200

    helloMongo

--- 10genEdu/M101/hellomongo.py
+++ 10genEdu/M101/hellomongo.py
@@ -5,5 +5,16 @@ import pymongo
 # this is the handler for the root address of the web server
 @bottle.route('/')
 def index():
-	
+    from pymongo import Connection
 
+    conn = Connection('localhost', 27017)
+    db = conn.patati
+    # db = conn.test
+    # names = db.names
+    names = db.nomi
+    item = names.find_one()
+
+    return '<b> Hello %s!<b>' %item['name']
+
+
+bottle.run(host='localhost', port=8082)
